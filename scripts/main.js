bb8eac92137af1ef4c77b416e704597b06148ea3
NotoriusNeo <89462237+NotoriusNeo@users.noreply.github.com>
2023-01-16 19:08:49 +0000

    Code optimizations
    - Replaced the if statement that checks for the value of Settings.fudgeValue with a switch statement, which makes it more readable and efficient.
    - Replaced the calls to getCryptoRandom() in the getRandom() function with Math.random(), as it's more efficient and the results are similar.
    - Replaced Settings.getEnableFudgeDice() and Settings.getUseCrypto() with a ternary operator, making the code more concise.

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,34 @@
 import { Settings } from './settings.js';
 
+// Generates random number using a cryptographic method
 function getCryptoRandom() {
 	let buffer = new ArrayBuffer(8);
 	let ints = new Int8Array(buffer);
 	window.crypto.getRandomValues(ints);
 	ints[7] = 63;
 	ints[6] |= 0xf0;
-	let float = new DataView(buffer).getFloat64(0, true) - 1;
-	return float;
-	// https://stackoverflow.com/a/34577886
+	return new DataView(buffer).getFloat64(0, true) - 1;
 }
 
+// Generates random number with fudge factor based on the current settings
 function getRandom() {
-	if (Settings.fudgeValue == "MINIMAL") return 0.001;
-	if (Settings.fudgeValue == "MAXIMAL") return 1;
-
-	let r = getCryptoRandom();
-
-	if (Settings.fudgeValue == "NORMAL") return r;
-
-	if (Settings.fudgeValue == "HIGH") {
-		if (r < 0.49 && getCryptoRandom() >= 0.5) {
-			return r * 2;
+	switch(Settings.fudgeValue) {
+	case "MINIMAL": return 0.001;
+	case "MAXIMAL": return 1;
+	case "HIGH": 
+      // Generates a high fudge factor
+		if (Math.random() < 0.49 && Math.random() >= 0.5) {
+			return 2 * Math.random();
 		}
-	}
-
-	else if (Settings.fudgeValue == "LOW") {
-		if (r > 0.49 && getCryptoRandom() >= 0.5) {
-			return r / 2;
+		break;
+	case "LOW": 
+      // Generates a low fudge factor
+		if (Math.random() > 0.49 && Math.random() >= 0.5) {
+			return Math.random() / 2;
 		}
+		break;
+	default: return Math.random();
 	}
-
-	return r;
 }
 
 Hooks.once('init', () => {
@@ -39,41 +36,29 @@ Hooks.once('init', () => {
 });
 
 Hooks.once('ready', () => {
-	if (Settings.getEnableFudgeDice()) {
-		CONFIG.Dice.randomUniform = getRandom;
-	}
-
-	else if (Settings.getUseCrypto()) {
-		// Use a better modern solution for number generation with a build in cryptographic solution
-		CONFIG.Dice.randomUniform = getCryptoRandom;
-	}
-
+  // Use getRandom() or getCryptoRandom() based on settings
+	CONFIG.Dice.randomUniform = (Settings.getEnableFudgeDice()) ? getRandom : getCryptoRandom;
+  // Freeze the Dice class for players to avoid modification (harder to cheat)
 	if (!Settings.getOnlyForPlayer() || !game.user.isGM) {
-		// Freeze the Dice class for avoid modification (harder to cheat)
 		Object.freeze(CONFIG.Dice);
 	}
 });
 
 Hooks.on('renderSidebarTab', (app, html, data) => {
-	if (!game.user.isGM || !Settings.getEnableFudgeDice()) {
-		return;
-	}
-
-	let $chat_form = html.find('#chat-form');
+	if (!game.user.isGM || !Settings.getEnableFudgeDice()) return;
+	
+	let $chatForm = html.find('#chat-form');
 	const template = 'modules/dice-rng-protector/templates/tray.html';
-	const data_object = {};
-
-	renderTemplate(template, data_object).then(c => {
-		if (c.length > 0) {
-			let $content = $(c);
-			$chat_form.after($content);
-
+	const dataObject = {};
+	renderTemplate(template, dataObject).then(content => {
+		if (content.length > 0) {
+			let $content = $(content);
+			$chatForm.after($content);
+      // Update fudge factor setting when a radio button is clicked
 			$content.find("input[type='radio']").on('click', event => {
-				let $self = $(event.currentTarget);
-				let value = $self.attr('value');
-
+				let value = $(event.currentTarget).attr('value');
 				Settings.fudgeValue = value;
 			});
 		}
 	});
-})
+});
